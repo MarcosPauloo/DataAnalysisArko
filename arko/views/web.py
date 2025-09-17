9f31656613ba0dcd46ae1976316bb0f739680703
Marcos Paulo de Oliveira Silva <marcos.silva4@arapiraca.ufal.br>
2025-09-17 15:27:24 -0300

    completed process adpated to Vue.js

--- arko/views/web.py
+++ arko/views/web.py
@@ -4,6 +4,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.core.serializers import serialize
+from django.core.serializers.json import DjangoJSONEncoder
 from ..models import State, Municipality, District, Company
 from ..filters import MunicipalityFilter, CompanyFilter, DistrictFilter, StateFilter
 
@@ -55,9 +56,19 @@ class MunicipalityListView(LoginRequiredMixin,ListView):
         context = super().get_context_data(**kwargs)
         context['filter'] = self.filter
 
-        # -- INJEÇÃO DE DADOS PARA O VUE.JS
-        municipality_list = list(self.filter.qs.values('id', 'nome', 'estado__nome'))
-        context['data_json'] = json.dumps(municipality_list)
+        paginator = context['paginator']
+        page_obj = context['page_obj']
+
+        municipality_list = list(page_obj.object_list.values('id', 'nome', 'estado__nome'))
+
+        initial_data = {
+            "count": paginator.count,
+            "next": page_obj.next_page_number() if page_obj.has_next() else None,
+            "previous": page_obj.previous_page_number() if page_obj.has_previous() else None,
+            "results": municipality_list
+        }
+
+        context['data_json'] = json.dumps(initial_data)
         return context
     
 class DistrictListView(LoginRequiredMixin,ListView):
@@ -75,13 +86,54 @@ class DistrictListView(LoginRequiredMixin,ListView):
         context = super().get_context_data(**kwargs)
         context['filter'] = self.filter
 
-        # -- INJEÇÃO DE DADOS PARA O VUE.JS
-        district_list = list(self.filter.qs.values('id', 'nome', 'municipio__nome', 'municipio__estado__nome'))
-        context['data_json'] = json.dumps(district_list)
+        paginator = context['paginator']
+        page_obj = context['page_obj']
+
+        district_list = list(page_obj.object_list.values('id', 'nome', 'municipio__nome', 'municipio__estado__nome'))
+
+        initial_data = {
+            "count": paginator.count,
+            "next": page_obj.next_page_number() if page_obj.has_next() else None,
+            "previous": page_obj.previous_page_number() if page_obj.has_previous() else None,
+            "results": district_list
+        }
+
+        context['data_json'] = json.dumps(initial_data)
 
         return context
     
-class CompanyListView(LoginRequiredMixin,ListView):
+# class CompanyListView(LoginRequiredMixin,ListView):
+#     model = Company
+#     template_name = 'arko/company_list.html'
+#     context_object_name = 'companies'
+#     paginate_by = 25
+
+#     def get_queryset(self):
+#         queryset = super().get_queryset()
+#         self.filter = CompanyFilter(self.request.GET, queryset=queryset)
+#         return self.filter.qs
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['filter'] = self.filter
+
+#         paginator = context['paginator']
+#         page_obj = context['page_obj']
+
+#         company_list = serialize('json', page_obj.object_list, fields=('razao_social','natureza_juridica', 'porte_empresa', 'capital_social'))
+
+#         initial_data = f'''{{
+#             "count": {paginator.count},
+#             "next": {json.dumps(page_obj.next_page_number() if page_obj.has_next() else None)},
+#             "previous": {json.dumps(page_obj.previous_page_number() if page_obj.has_previous() else None)},
+#             "results": {company_list}
+#         }}'''
+
+#         context['data_json'] = json.dumps(initial_data) 
+
+#         return context
+
+class CompanyListView(LoginRequiredMixin, ListView):
     model = Company
     template_name = 'arko/company_list.html'
     context_object_name = 'companies'
@@ -95,11 +147,35 @@ class CompanyListView(LoginRequiredMixin,ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['filter'] = self.filter
-
-        context['data_json'] = serialize('json', self.filter.qs, fields=('cnpj', 'razao_social', 'porte_empresa'))
-
+        
+        # --- INJEÇÃO DE DADOS CORRIGIDA E SIMPLIFICADA ---
+        paginator = context['paginator']
+        page_obj = context['page_obj']
+        
+        # Usamos .values() para pegar os dados que o frontend precisa de forma direta.
+        # Note que para a chave primária, usamos 'cnpj' que é o nome do campo no modelo.
+        company_list = list(page_obj.object_list.values(
+            'cnpj', 
+            'razao_social', 
+            'natureza_juridica', 
+            'porte_empresa', 
+            'capital_social'
+        ))
+        
+        # Criamos o dicionário que imita a resposta da API do DRF
+        initial_data = {
+            "count": paginator.count,
+            "next": page_obj.next_page_number() if page_obj.has_next() else None,
+            "previous": page_obj.previous_page_number() if page_obj.has_previous() else None,
+            "results": company_list
+        }
+        
+        # Usamos o DjangoJSONEncoder para converter corretamente o campo Decimal
+        context['data_json'] = json.dumps(initial_data, cls=DjangoJSONEncoder)
+        # --- FIM DA INJEÇÃO ---
+        
         return context
-    
+
 def logout_view(request):
     logout(request)
 
